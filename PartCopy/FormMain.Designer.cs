c8a2d4e82101af88dd6bf0c2c2620dd300e3df39
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2016-09-25 16:49:56 +0000

    start
    git-svn-id: svn://192.168.3.99/PartCopy@6166 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ PartCopy/FormMain.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace PartCopy
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtDest = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.udStart = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.udEnd = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.udStart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udEnd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSource
+            // 
+            this.txtSource.AllowDrop = true;
+            this.txtSource.Location = new System.Drawing.Point(79, 25);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(706, 19);
+            this.txtSource.TabIndex = 0;
+            this.txtSource.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtSource_DragDrop);
+            this.txtSource.DragEnter += new System.Windows.Forms.DragEventHandler(this.txt_DragEnter);
+            this.txtSource.DragOver += new System.Windows.Forms.DragEventHandler(this.txt_DragOver);
+            // 
+            // txtDest
+            // 
+            this.txtDest.AllowDrop = true;
+            this.txtDest.Location = new System.Drawing.Point(79, 67);
+            this.txtDest.Name = "txtDest";
+            this.txtDest.Size = new System.Drawing.Size(706, 19);
+            this.txtDest.TabIndex = 0;
+            this.txtDest.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtDest_DragDrop);
+            this.txtDest.DragEnter += new System.Windows.Forms.DragEventHandler(this.txt_DragEnter);
+            this.txtDest.DragOver += new System.Windows.Forms.DragEventHandler(this.txt_DragOver);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "&Source";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "&Dest";
+            // 
+            // udStart
+            // 
+            this.udStart.Location = new System.Drawing.Point(79, 122);
+            this.udStart.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.udStart.Name = "udStart";
+            this.udStart.Size = new System.Drawing.Size(274, 19);
+            this.udStart.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "S&tart:";
+            // 
+            // udEnd
+            // 
+            this.udEnd.Location = new System.Drawing.Point(79, 147);
+            this.udEnd.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.udEnd.Name = "udEnd";
+            this.udEnd.Size = new System.Drawing.Size(274, 19);
+            this.udEnd.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "&End:";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(81, 219);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(154, 23);
+            this.btnCopy.TabIndex = 4;
+            this.btnCopy.Text = "&Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnClose.Location = new System.Drawing.Point(680, 295);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(105, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "&OK";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // FormMain
+            // 
+            this.AcceptButton = this.btnClose;
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 330);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.udEnd);
+            this.Controls.Add(this.udStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDest);
+            this.Controls.Add(this.txtSource);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "PartCopy";
+            ((System.ComponentModel.ISupportInitialize)(this.udStart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udEnd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtDest;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown udStart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown udEnd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ PartCopy/FormMain.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PartCopy
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_DragEnter(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void txt_DragOver(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void txtSource_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                object[] o = (object[])e.Data.GetData(DataFormats.FileDrop);
+                txtSource.Text = o[0].ToString();
+
+            }
+
+        }
+
+        private void txtDest_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                object[] o = (object[])e.Data.GetData(DataFormats.FileDrop);
+                txtDest.Text = o[0].ToString();
+
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.IO.FileStream fsr = new System.IO.FileStream(
+                    txtSource.Text,
+                    System.IO.FileMode.Open,
+                    System.IO.FileAccess.Read);
+
+                int startbyte = (int)udStart.Value;
+                int endbyte = (int)udEnd.Value;
+                int size = endbyte - startbyte;
+
+                byte[] bs = new byte[size];
+
+                fsr.Read(bs, startbyte, size);
+                fsr.Close();
+
+
+                FileStream fsw = new FileStream(
+                    txtDest.Text,
+                    FileMode.Open,
+                    FileAccess.Write);
+
+                fsw.Write(bs, startbyte, size);
+                fsw.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,
+                    Application.ProductName,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+  
+
+    }
+}
